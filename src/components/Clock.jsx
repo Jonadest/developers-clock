2e3d52904ad7006b27ad0e7be47d3e2a41a053a2
Ifeanyi <140451177+Jonadest@users.noreply.github.com>
2024-07-01 07:42:53 +0100

    removed seconds

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -8,13 +8,13 @@ const Clock = () => {
       const now = new Date();
       let hours = now.getHours();
       const minutes = now.getMinutes().toString().padStart(2, "0");
-      const seconds = now.getSeconds().toString().padStart(2, "0");
+      //const seconds = now.getSeconds().toString().padStart(2, "0");
       const ampm = hours >= 12 ? "PM" : "AM";
 
       hours = hours % 12 || 12; // Convert to 12-hour format, '0' becomes '12'
       hours = hours.toString().padStart(2, "0");
 
-      setTime(`${hours}:${minutes}:${seconds} ${ampm}`);
+      setTime(`${hours} : ${minutes} ${ampm}`);
     };
 
     const intervalId = setInterval(updateClock, 1000);
