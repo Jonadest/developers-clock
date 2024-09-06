e044bdc5d8fe2531b88ff70242703287784da928
Ifeanyi <140451177+Jonadest@users.noreply.github.com>
2024-09-06 01:24:22 +0100

    Vite for safari

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import legacy from "@vitejs/plugin-legacy";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    legacy({
-      targets: ["defaults", "not IE 11"],
-    }),
     react(),
+    legacy({
+      targets: ["defaults", "not IE 11", "Safari >= 10"], // Ensure support for Safari 10+
+    }),
   ],
   server: {
     host: "192.168.8.199",
-    port: "5500",
+    port: 3000,
   },
 });
