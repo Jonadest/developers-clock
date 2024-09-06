d591675868c98b685a5e17c43bae278358ca091f
Ifeanyi <140451177+Jonadest@users.noreply.github.com>
2024-09-06 01:05:57 +0100

    Updated to yarn

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import legacy from "@vitejs/plugin-legacy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    legacy({
+      targets: ["defaults", "not IE 11"],
+    }),
+    react(),
+  ],
   server: {
     host: "192.168.8.199",
-    port: "3000",
+    port: "5500",
   },
 });
